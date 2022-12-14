53aef27cf6f36448721791c9004450908645a428
Android 101 <Android101@int.adtspb.ru>
2022-12-14 18:16:01 +0300

    first correction

---
+++ app/src/main/java/com/example/progressbar/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.progressbar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.progressbar.ui.theme.ProgressBarTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+       
+                }
+            }
+        }
